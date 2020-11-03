78e20187c97d8b1d35439afc21d5231fb5c31307
nick-sib <khc1981@mail.ru>
2020-11-04 01:42:59 +0700

    Start code from lesson doc

---
+++ app/src/main/java/com/nick_sib/popularlibraries/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.nick_sib.popularlibraries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), MainView  {
+
+    private val presenter = MainPresenter(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listener = View.OnClickListener {
+            presenter.counterClick(it.id)
+        }
+
+        btn_counter1.setOnClickListener(listener)
+        btn_counter2.setOnClickListener(listener)
+        btn_counter3.setOnClickListener(listener)
+    }
+
+    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
+    override fun setButtonText(index: Int, text: String) {
+        when(index){
+            0 -> btn_counter1.text = text
+            1 -> btn_counter2.text = text
+            2 -> btn_counter3.text = text
+        }
+    }
+}
---
+++ app/src/main/java/com/nick_sib/popularlibraries/MainView.kt
@@ -0,0 +1,5 @@
+package com.nick_sib.popularlibraries
+
+interface MainView {
+    fun setButtonText(index: Int, text: String)
+}
