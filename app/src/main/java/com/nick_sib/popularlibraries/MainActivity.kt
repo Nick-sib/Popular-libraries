c54519e95371efb9ad3b59e724ab3a42841f6b4a
nick-sib <khc1981@mail.ru>
2020-11-04 01:53:11 +0700

    Made the code testable

--- app/src/main/java/com/nick_sib/popularlibraries/MainActivity.kt
+++ app/src/main/java/com/nick_sib/popularlibraries/MainActivity.kt
@@ -7,27 +7,30 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainView  {
 
+    private val buttons by lazy {
+        listOf(btn_counter1, btn_counter2, btn_counter3)
+    }
+
     private val presenter = MainPresenter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val listener = View.OnClickListener {
-            presenter.counterClick(it.id)
-        }
-
-        btn_counter1.setOnClickListener(listener)
-        btn_counter2.setOnClickListener(listener)
-        btn_counter3.setOnClickListener(listener)
     }
 
-    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
-    override fun setButtonText(index: Int, text: String) {
-        when(index){
-            0 -> btn_counter1.text = text
-            1 -> btn_counter2.text = text
-            2 -> btn_counter3.text = text
+    override fun onResume() {
+        super.onResume()
+
+        buttons.forEach { button ->
+            val index = buttons.indexOf(button)
+            button.setOnClickListener {
+                presenter.counterClick(index)
+            }
         }
+
+    }
+
+    override fun setButtonText(index: Int, text: String) {
+        buttons[index].text = text
     }
 }
