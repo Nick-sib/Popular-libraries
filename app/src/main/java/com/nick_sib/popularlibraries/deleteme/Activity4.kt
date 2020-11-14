913e50ea28913a457c69cc16631c80b71956b1a2
nick-sib <khc1981@mail.ru>
2020-11-14 23:04:58 +0700

    Lesson 4 base

---
+++ app/src/main/java/com/nick_sib/popularlibraries/deleteme/Activity4.kt
@@ -0,0 +1,18 @@
+package com.nick_sib.popularlibraries.deleteme
+
+import android.os.Bundle
+import com.nick_sib.popularlibraries.R
+import kotlinx.android.synthetic.main.activity_4.*
+import moxy.MvpAppCompatActivity
+
+class Activity4 : MvpAppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_4)
+
+        bOpenImage.setOnClickListener {
+
+        }
+    }
+}
